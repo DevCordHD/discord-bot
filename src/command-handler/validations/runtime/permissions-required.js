b05189b18ad030f422495ae2dbce307de33efafb
DevCord <devcordtv@proton.me>
2022-12-03 10:34:17 +0300

    return false from permissions required

--- src/command-handler/validations/runtime/permissions-required.js
+++ src/command-handler/validations/runtime/permissions-required.js
@@ -2,7 +2,11 @@ module.exports = async ({ client, command, interaction }) => {
   if (command.permissionsRequired.length) {
     for (const permission of command.permissionsRequired) {
       if (!interaction.member.permissions.has(permission)) {
-        return interaction.reply(`Not enough permissions.`);
+        interaction.reply({
+          content: 'Not enough permissions.',
+          ephemeral: true,
+        });
+        return false;
       }
     }
   }
